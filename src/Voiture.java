364b8254406a0c571d59503fe0c8188d7c216e99
cplantet <cedric.plantet@gmail.com>
2020-05-19 11:37:00 +0200

    Classe Voiture créée

---
+++ src/Voiture.java
@@ -0,0 +1,17 @@
+public class Voiture {
+    private int nbPlaces;
+
+    public Voiture(int nbPlaces){
+        super();
+        this.nbPlaces = 0;
+
+    }
+
+    public int getNbPlaces() {
+        return nbPlaces;
+    }
+
+    public void setNbPlaces(int nbPlaces) {
+        this.nbPlaces = nbPlaces;
+    }
+}
